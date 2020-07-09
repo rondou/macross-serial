c788a1af0ad7342e4f24d3f2824296d32298f5d1
Henry Chang <jrhenry@flotechnologies.com>
2020-07-09 23:38:56 +0800

    Improve the IPC interface

--- bin/ipc-tunnel-server.py
+++ bin/ipc-tunnel-server.py
@@ -1,21 +1,46 @@
 import multiprocessing
 import multiprocessing.managers
+from typing import List
 
 
-class MyManager(multiprocessing.managers.BaseManager):
-    pass
+q: List[multiprocessing.Queue] = list()
 
 
-def get_queue():
+class QueueManager(multiprocessing.managers.BaseManager):
+
+    def get_queue(self) -> multiprocessing.Queue:
+        pass
+
+
+delattr(QueueManager, 'get_queue')
+
+
+def get_queue() -> multiprocessing.Queue:
     global q
-    return q
+    if not q:
+        q.append(multiprocessing.Queue())
+    return q[0]
 
 
-q = multiprocessing.Queue()
-MyManager.register('queue', get_queue)
+def init_ipc_queue_manager_client():
+    if not hasattr(QueueManager, 'get_queue'):
+        QueueManager.register('get_queue')
+
+
+def init_ipc_queue_manager_server():
+    if not hasattr(QueueManager, 'get_queue'):
+        QueueManager.register('get_queue', get_queue)
+
 
 if __name__ == '__main__':
-    with MyManager(authkey='macross_serial'.encode()) as manager:
+    init_ipc_queue_manager_server()
+
+    manager: QueueManager
+    with QueueManager(authkey='macross_serial'.encode()) as manager:
         print(f"Server address: {manager.address}")
         while True:
-            print(f"{manager.queue().get()}")
+            print(f"{manager.get_queue().get()}")
+
+
+# Reference and Usage:
+#   https://gist.github.com/changyuheng/89062d639e40110c61c2f88018a8b0e5
